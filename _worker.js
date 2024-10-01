890878d8cd285ff28d28bd9f9b212e137533bbc1
mahsanat <143261075+mahsanat@users.noreply.github.com>
2024-10-01 16:48:07 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '474a8a20-bd33-413c-a3ba-9616a37cc7a1';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
